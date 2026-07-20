898771957367813d3629e39a2362729ef6ab286d
SO-SO <sys107502@gmail.com>
2026-07-20 17:33:17 +0900

    Feat : 404 페이지 구현

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,16 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../components/layout/MainLayout";
 import Dashboard from "../pages/Dashboard";
+import NotFoundPage from "../pages/NotFoundPage";
 
 export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout />,
-    children: [
-      { index: true, element: <Dashboard /> },
-      { path: "/retrospect", element: <Dashboard /> },
-      { path: "/stats", element: <Dashboard /> },
-      { path: "/mypage", element: <Dashboard /> },
-    ],
-  },
+    {
+        path: "/",
+        element: <MainLayout />,
+        children: [
+            { index: true, element: <Dashboard /> },
+            { path: "/retrospect", element: <Dashboard /> },
+            { path: "/stats", element: <Dashboard /> },
+            { path: "/mypage", element: <Dashboard /> },
+            { path: "*", element: <NotFoundPage /> },
+        ],
+    },
 ]);
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,42 +4,42 @@ import { Panel } from "../components/common/Panel";
 import PomodoroTimer from "../components/pomodoro/PomodoroTimer";
 
 export default function Dashboard() {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-7 gap-4">
-      {/* 왼쪽 열  : 날씨랑 할일목록 */}
-      <div className="flex flex-col col-span-2 gap-4">
-        <Panel variant="neumorphism" className="p-4">
-          날씨
-        </Panel>
-        <Panel variant="neumorphism" className="p-4">
-          할일목록
-        </Panel>
-      </div>
-      {/* 가운데 열 : 뽀모도로 타이머랑 위에 는 미정 */}
-      <div className="flex flex-col col-span-3 gap-4">
-        <Panel variant="neumorphism" className="p-4">
-          미정
-        </Panel>
-        <Panel variant="neumorphism" className="p-4">
-          <PomodoroTimer />
-        </Panel>
-      </div>
-      {/* 오른쪽 열 : 잔디심기랑 밸런스 케어 (카페인 수분) + 선인장 */}
-      <div className="flex flex-col col-span-2 gap-4">
-        <Panel variant="neumorphism" className="p-4">
-          잔디심기
-        </Panel>
-        <Panel variant="neumorphism" className="p-4">
-          선인장
-        </Panel>
-      </div>
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-7 gap-4">
+            {/* 왼쪽 열  : 날씨랑 할일목록 */}
+            <div className="flex flex-col col-span-2 gap-4">
+                <Panel variant="neumorphism" className="p-4">
+                    날씨
+                </Panel>
+                <Panel variant="neumorphism" className="p-4">
+                    할일목록
+                </Panel>
+            </div>
+            {/* 가운데 열 : 뽀모도로 타이머랑 위에 는 미정 */}
+            <div className="flex flex-col col-span-3 gap-4">
+                <Panel variant="neumorphism" className="p-4">
+                    미정
+                </Panel>
+                <Panel variant="neumorphism" className="p-4">
+                    <PomodoroTimer />
+                </Panel>
+            </div>
+            {/* 오른쪽 열 : 잔디심기랑 밸런스 케어 (카페인 수분) + 선인장 */}
+            <div className="flex flex-col col-span-2 gap-4">
+                <Panel variant="neumorphism" className="p-4">
+                    잔디심기
+                </Panel>
+                <Panel variant="neumorphism" className="p-4">
+                    선인장
+                </Panel>
+            </div>
 
-      {/* 뮤직 플레이어 */}
-      <div className="col-span-7">
-        <Panel variant="neumorphism" className="p-4">
-          뮤직 플레이어
-        </Panel>
-      </div>
-    </div>
-  );
+            {/* 뮤직 플레이어 */}
+            <div className="col-span-7">
+                <Panel variant="neumorphism" className="p-4">
+                    뮤직 플레이어
+                </Panel>
+            </div>
+        </div>
+    );
 }
