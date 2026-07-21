2aa242f30c422f9fa8d07213d06b463359ed2708
daenggg <dywjd690108@naver.com>
2026-07-21 10:02:52 +0900

    Style: 글로벌 콕핏 레이아웃 프레임 적용

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -4,10 +4,10 @@ import Header from "../common/Header";
 // [components/layout] Header, Sidebar 등 전체 화면 구조(뼈대)를 구성하는 컴포넌트를 담는 공간입니다.
 export default function MainLayout() {
   return (
-    <div className="min-h-screen bg-bg-light">
-      <div className="max-w-7xl mx-auto">
+    <div className="w-screen h-screen flex flex-col bg-bg-light overflow-hidden">
+      <div className="max-w-7xl mx-auto w-full h-full flex flex-col overflow-hidden px-4">
         <Header />
-        <main className=" p-4 pt-6">
+        <main className="flex-1 overflow-hidden p-4">
           <Outlet />
         </main>
       </div>
