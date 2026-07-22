d3d385250b2c2559d603ab0ad87fc7d879d2f452
DongHYEON <85489985+donghyeon01@users.noreply.github.com>
2026-07-22 21:27:50 +0900

    Feat :  헤더 및 대시보드
    - 대시보드 3컬럼 3단 반응형 설정
    - 헤더 높이, 로고 이미지, 반응형 설정
    
    => 현재 반응형 grid 에서 flex로 수정 예정

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -5,19 +5,26 @@ import PomodoroTimer from "../components/pomodoro/PomodoroTimer";
 
 export default function Dashboard() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-7 gap-4">
+    <div className="grid w-full h-full grid-cols-1 gap-6 max-[819px]:h-auto max-[819px]:content-start min-[600px]:grid-cols-[290px_minmax(0px,1fr)] min-[820px]:grid-cols-4">
       {/* 왼쪽 열  : 날씨랑 할일목록 */}
-      <div className="flex flex-col col-span-2 gap-4">
-        <Panel variant="neumorphism" className="p-4">
+      <div className="flex min-h-0 flex-col gap-6 ">
+        <Panel variant="neumorphism" className="min-h-0 flex-1 p-4">
           날씨
         </Panel>
-        <Panel variant="neumorphism" className="p-4">
+        <Panel variant="neumorphism" className="min-h-0 flex-1 p-4">
           할일목록
         </Panel>
       </div>
       {/* 가운데 열 : 뽀모도로 타이머랑 위에 는 미정 */}
-      <div className="flex flex-col col-span-3 gap-4">
-        <Panel variant="neumorphism" className="p-4">
+      {/* 820px 미만(단일 컬럼)에서는 이 열을 최상단으로 이동 */}
+      <div className="flex min-h-[284px] flex-col gap-6 order-first min-[820px]:order-none min-[820px]:col-span-2">
+        {/* 820px 미만에서는 뽀모도로 타이머를 먼저 표시 */}
+        <Panel
+          variant="neumorphism"
+          className="min-h-[380px] min-w-[290px] flex-1 p-2 order-first min-[820px]:order-none">
+          <PomodoroTimer />
+        </Panel>
+        <Panel variant="neumorphism" className="min-h-0 flex-1 p-4">
           <div className="background-container">
             <div className="liquid-glass">
               {/* 텍스트나 아이콘을 넣을 수 있습니다 */}
@@ -25,24 +32,14 @@ export default function Dashboard() {
             </div>
           </div>
         </Panel>
-        <Panel variant="neumorphism" className="p-4">
-          <PomodoroTimer />
-        </Panel>
       </div>
       {/* 오른쪽 열 : 잔디심기랑 밸런스 케어 (카페인 수분) + 선인장 */}
-      <div className="flex flex-col col-span-2 gap-4">
-        <Panel variant="neumorphism" className="p-4">
+      <div className="flex min-h-0 flex-col gap-6 max-[819px]:col-span-2 max-[599px]:col-span-1">
+        <Panel variant="neumorphism" className="min-h-0 flex-1 p-4">
           잔디심기
         </Panel>
-        <Panel variant="neumorphism" className="p-4">
+        <Panel variant="neumorphism" className="min-h-0 flex-1 p-4">
           선인장
-        </Panel>
-      </div>
-
-      {/* 뮤직 플레이어 */}
-      <div className="col-span-7">
-        <Panel variant="neumorphism" className="p-4">
-          뮤직 플레이어
         </Panel>
       </div>
     </div>
