da0a48d3c19c9c25cfd5b2e28d876f2e1709e10c
SO-SO <sys107502@gmail.com>
2026-07-23 09:32:04 +0900

    Fix:빌드 오류 수정

--- src/components/mypage/MusicController.tsx
+++ src/components/mypage/MusicController.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Panel } from "../common/Panel";
 
 export const MusicController = () => {
--- src/components/mypage/YearSelector.tsx
+++ src/components/mypage/YearSelector.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export const YearSelector = () => {
     return <div>YearSelector</div>;
 };
