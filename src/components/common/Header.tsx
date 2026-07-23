f3808b9144863586e6e35945e2fe24ac100fc0ea
daenggg <dywjd690108@naver.com>
2026-07-23 10:56:11 +0900

    fix: 미사용 임포트 제거로 빌드 에러 해결

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,6 +1,5 @@
 import { useState, useRef, useEffect } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
-import { toast } from "react-hot-toast";
 
 import { Panel } from "./Panel";
 import { RestrictedArea } from "./RestrictedArea";
