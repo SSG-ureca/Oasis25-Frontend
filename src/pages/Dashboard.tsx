d8e177fd03ed91cc46847b2bf1fc53c0928f7b4d
donghyeon01 <85489985+donghyeon01@users.noreply.github.com>
2026-07-28 13:30:56 +0900

    Style : Dashboard
    명언 패널 max 값 추가

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -43,7 +43,7 @@ export default function Dashboard() {
           <PomodoroTimer onFocusModeChange={setIsFocusMode} />
         </Panel>
         {!isFocusMode && (
-          <Panel variant="clay" className="min-w-[290px] flex-1 p-4">
+          <Panel variant="clay" className="min-w-[290px] max-h-25 flex-1 p-2">
             <div className="background-container">
               <div className="liquid-glass">
                 {/* 텍스트나 아이콘을 넣을 수 있습니다 */}
